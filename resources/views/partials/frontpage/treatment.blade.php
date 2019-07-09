124ee00c1c4dfb75b0d7bd1a10095664e843f185
Olle <me@olle.io>
2019-07-09 16:26:38 +0200

    ADD: Refactor treatment layout

--- resources/views/partials/frontpage/treatment.blade.php
+++ resources/views/partials/frontpage/treatment.blade.php
@@ -4,17 +4,19 @@
     <h2 class="ml-8">{!! $treatment['heading'] !!}</h2>
   </div>
 
-  <div class="flex flex-wrap justify-center">
+  <div class="flex flex-col justify-center">
     @foreach ($treatment['blurbs'] as $blurb)
-      <div class="w-1/2 p-8">
+      <div class="w-full flex mb-8">
+        <div class="w-1/3 flex flex-col justify-center text-box bg-blue-lightest px-6 py-8">
+          <div class="mb-6">
+            <h3 class="text-2xl mb-4">{!! $blurb['title'] !!}</h3>
+            <p class="text-base">{!! $blurb['description'] !!}</p>
+          </div>
+        </div>
         <div
-          class="fh-img bg-cover bg-center"
+          class="fh-img w-2/3 bg-cover bg-center"
           style="background-image: url('{{ $blurb['image'] }}');"
         >
-        </div>
-        <div class="text-box bg-blue-lightest shadow-lg w-5/6 mx-auto px-6 py-8">
-          <h3 class="text-2xl mb-4">{!! $blurb['title'] !!}</h3>
-          <p class="text-base">{!! $blurb['description'] !!}</p>
         </div>
       </div>
     @endforeach
