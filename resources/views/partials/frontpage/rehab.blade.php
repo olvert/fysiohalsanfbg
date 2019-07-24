116b4c43c914c2068cd5949e74113b1ad58f9f7f
Olle <me@olle.io>
2019-07-24 18:45:24 +0200

    FIX: Some margin/padding

--- resources/views/partials/frontpage/rehab.blade.php
+++ resources/views/partials/frontpage/rehab.blade.php
@@ -1,11 +1,11 @@
-<section id="rehab" class="bg-blue-lightest mb-0 pb-24">
-  <div class="container pb-8">
+<section id="rehab" class="bg-blue-lightest mb-0">
+  <div class="container">
     <div class="w-4/5 py-24">
       <div class="flex items-center mb-12">
         <span class="fh-dash-y"></span>
         <h2 class="ml-8">{!! $rehab['heading'] !!}</h2>
       </div>
-      
+
       <p class="mb-12">{!! $rehab['text'] !!}</p>
 
       <div class="aspect-ratio-box aspect-ratio-16x9">
