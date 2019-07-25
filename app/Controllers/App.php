c3aa92dbd7dbd9325a295d4b0822ccda77273108
Olle <me@olle.io>
2019-07-25 15:43:17 +0200

    WIP: Finished template for cookie policy

--- app/Controllers/App.php
+++ app/Controllers/App.php
@@ -30,4 +30,8 @@ class App extends Controller
         }
         return get_the_title();
     }
+
+    public static function cookiePolicy() {
+        return get_field('cookie_policy', 'option');
+    }
 }
---
+++ resources/views/partials/cookie-notice.blade.php
@@ -0,0 +1,12 @@
+<div id="cookie-policy" class="fixed bottom-0 inset-x-0 bg-blue-lightest shadow-lg">
+  <div class="py-6 px-6 sm:px-10 flex flex-col sm:flex-row justify-between items-center">
+    <p class="text-sm sm:text-base lg:text-lg sm:pr-8">
+      {!! App::cookiePolicy() !!}
+    </p>
+    <div class="w-full sm:w-auto text-right mt-4 sm:mt-0">
+      <button id="accept-cookies">
+        Fortsätt
+      </button>
+    <div>
+  </div>
+</div>
