d0a149240ad4ab5bcb74c1f35e74a10edd3f24ec
Olle <me@olle.io>
2019-07-25 12:30:40 +0200

    ADD: Minor fixes on mobile header and menu

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,13 +1,13 @@
 <header class="banner fixed shadow lg:shadow-none lg:relative bg-white top-0 inset-x-0 z-50">
-  <div class="h-24 pt-6 lg:pt-8">
+  <div class="h-16 sm:h-24 pt-3 sm:pt-6 lg:pt-8">
     <div class="container flex justify-between items-center">
-      <img class="w-16 lg:w-24 flex-none" src="@asset('images/logotypes/fysiohalsan_main.png')">
+      <img class="w-12 sm:w-16 lg:w-24 flex-none" src="@asset('images/logotypes/fysiohalsan_main.png')">
       <nav class="nav-primary-desktop hidden lg:inline-block -mr-4">
         @if (has_nav_menu('primary_navigation'))
           {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
         @endif
       </nav>
-      <button id="open-menu" class="inline-block lg:hidden">
+      <button id="open-menu" class="inline-block mt-2 lg:hidden">
         Meny
       </button>
     </div>
