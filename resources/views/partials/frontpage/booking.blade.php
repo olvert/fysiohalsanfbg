963eea58ee9e2be98adbeb5c34992f7097a85344
Olle <me@olle.io>
2019-07-25 12:05:59 +0200

    ADD: Booking responsive

--- resources/views/partials/frontpage/booking.blade.php
+++ resources/views/partials/frontpage/booking.blade.php
@@ -1,24 +1,24 @@
 <section id="bokning" class="bg-blue-lightest mb-0">
   <div class="container pb-8">
-    <div class="w-4/5 py-24">
-      <div class="flex items-center justify-between mb-12">
+    <div class="w-full lg:w-4/5 py-12 sm:py-20 lg:py-24">
+      <div class="flex items-center justify-between mb-8 sm:mb-12">
         <div class="flex items-center">
           <span class="fh-dash-y"></span>
-          <h2 class="ml-8">{!! $booking['heading'] !!}</h2>
+          <h2 class="ml-6 sm:ml-8">{!! $booking['heading'] !!}</h2>
         </div>
         <a
-          class="fh-book-button bg-blue-light text-2xl text-black no-underline font-playfair px-8 py-4 flex items-center"
+          class="fh-book-button bg-blue-light text-lg sm:text-xl lg:text-2xl text-black no-underline font-playfair px-6 sm:px-8 py-3 sm:py-4 flex items-center"
           href="{{ $book_button['link'] }}"
           target="_blank"
         >
           <span>{{ $book_button['text'] }}</span>
-          <img class="w-5 ml-4" src="@asset('images/icons/external-link.svg')">
+          <img class="w-5 ml-3 sm:ml-4" src="@asset('images/icons/external-link.svg')">
         </a>
       </div>
 
       <div>
         @foreach ($booking['blocks'] as $block)
-          <div class="mb-12">
+          <div class="mb-8 sm:mb-12">
             <div class="flex items-center">
               <span class="fh-dash-x-small mr-4"></span>
               <h4 class="mt-1">{!! $block['subheading'] !!}</h4>
